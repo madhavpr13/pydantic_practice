737a29ba1009b9c38a9b9b3c771e3e7a5eb57417
Madhav <madhav@zancompute.com>
2023-12-21 16:30:48 +0530

    Modified id as isbn, added Config json schema extra and field constraints

--- fastapi/src/app.py
+++ fastapi/src/app.py
@@ -1,23 +1,38 @@
 from fastapi import FastAPI
 from books import load_books
+from books import Book, BookRequest
 
 BOOKS = load_books()
 
 app = FastAPI()
 
+
 @app.get("/")
 def say_hello() -> str:
     return "Hello, World"
+
 
 @app.get("/books")
 async def get_books() -> list[dict]:
     return BOOKS
 
 
-@app.get("/books/{id}")
-async def get_book_id(id: int) -> dict:
+@app.get("/books/{isbn}")
+async def get_book_id(isbn: str) -> dict:
     for book_dict in BOOKS:
-        if book_dict["id"] == id:
+        if book_dict["isbn"] == isbn:
             return book_dict
     return dict()
 
+
+@app.post("/create_book")
+async def create_book(book_request: BookRequest) -> None:
+    print(f"typeof book_request: {type(book_request)}")
+    new_book = book_request.model_dump()
+    BOOKS.append(new_book)
+
+
+if __name__ == "__main__":
+    book_request = BookRequest.model_validate(BOOKS[0])
+    print(book_request.model_dump(by_alias=True))
+    
--- fastapi/src/books.py
+++ fastapi/src/books.py
@@ -3,15 +3,21 @@ from pydantic import Field
 from pydantic.alias_generators import to_camel
 from typing import List, Dict
 
+
 def load_books():
     import json
+
     with open("../files/books.json", "r") as f:
         books = json.load(f)
     return books
 
-class BookRequest(BaseModel):
-    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
-    id_ : int = Field(validation_alias="id", serialization_alias="id")
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Book:
+    isbn: str
     title: str
     author: str
     genre: str
@@ -20,9 +26,34 @@ class BookRequest(BaseModel):
     rating: float
 
 
-if __name__ =="__main__":
+class BookRequest(BaseModel):
+    # model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
+    isbn: int = Field(validation_alias="ISBN", serialization_alias="ISBN")
+    title: str
+    author: str
+    genre: str
+    description: str
+    year_of_publication: int
+    rating: float = Field(ge=0, le=5)
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "isbn": "978-3-16-148410-0",
+                "title": "The Hobbit",
+                "author": "J.R.R Tolkien",
+                "genre": "Fantasy",
+                "description": "The Hobbit, or There and Back Again is a children's fantasy novel by English author J. R. R. Tolkien.",
+                "year_of_publication": 1937,
+                "rating": 4.8,
+            }
+        }
+
+
+if __name__ == "__main__":
     import json
     import random
+
     with open("../files/books.json", "r") as f:
         books = json.load(f)
     random_book = BookRequest.model_validate(random.choice(books))
